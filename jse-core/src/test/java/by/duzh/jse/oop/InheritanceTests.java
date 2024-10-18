7e619a411ab0ddce0d2427f17205bb6e4870a967
vduzh <vduzh@tut.by>
2024-10-18 17:59:04 +0300

    updated

---
+++ jse-core/src/test/java/by/duzh/jse/oop/InheritanceTests.java
@@ -0,0 +1,10 @@
+package by.duzh.jse.oop;
+
+import org.junit.Test;
+
+public class InheritanceTests {
+    @Test
+    public void test() {
+        System.out.println("test is working....");
+    }
+}
