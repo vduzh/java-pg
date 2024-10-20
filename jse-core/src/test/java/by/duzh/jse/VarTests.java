5a96b9f627f2bd575ec292233d3cc8aa99956541
vduzh <vduzh@tut.by>
2024-10-20 22:03:17 +0300

    updated

---
+++ jse-core/src/test/java/by/duzh/jse/VarTests.java
@@ -0,0 +1,13 @@
+package by.duzh.jse;
+
+import org.junit.Test;
+
+public class VarTests {
+    @Test
+    public void testCreate() throws Exception {
+        // standard approach
+        long a = 1;
+        // with var keyword
+        var b = 1L;
+    }
+}
