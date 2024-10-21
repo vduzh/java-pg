ce8fa252b458c4f899c9eb693bddf2c2d5dce7fc
vduzh <vduzh@tut.by>
2024-10-21 12:06:11 +0300

    updated

---
+++ jse-core/src/test/java/by/duzh/jse/CommandLineArgsTests.java
@@ -0,0 +1,18 @@
+package by.duzh.jse;
+
+import org.junit.Test;
+
+class Application {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+    }
+}
+
+public class CommandLineArgsTests {
+    @Test
+    public void testArguments() throws Exception {
+        Application.main(new String[]{"foo", "bar"});
+    }
+}
