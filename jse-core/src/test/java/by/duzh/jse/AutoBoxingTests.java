9dbdcbe3c94456842d32a388ff6f18551a1389cc
vduzh <vduzh@tut.by>
2024-10-22 18:40:01 +0300

    added

---
+++ jse-core/src/test/java/by/duzh/jse/AutoBoxingTests.java
@@ -0,0 +1,63 @@
+package by.duzh.jse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+public class AutoBoxingTests {
+    @Test
+    public void manualBoxingAndUnboxing() throws Exception {
+        // boxing
+        Integer intObj = Integer.valueOf(100);
+
+        // unboxing
+        int i = intObj.intValue();
+    }
+
+    @Test
+    public void autoBoxingAndUnboxing() {
+        // a compiler does that automatically
+
+        // auto boxing
+        Integer intObj = 100;
+
+        // auto unboxing
+        int i = intObj;
+    }
+
+    @Test
+    public void withMethods() {
+        class AutoBox {
+            static int sum(Integer i, int j) {
+                return i + j;
+            }
+        }
+
+        // use autoboxing on the methods params
+        final var j = Integer.valueOf(20);
+        Integer res = AutoBox.sum(10, j);
+
+        Assert.assertEquals(30, res.intValue());
+    }
+
+    @Test
+    public void inExpressions() {
+        // use autoboxing
+        final var intObj = Integer.valueOf(20);
+        Integer res = 10 + intObj;
+
+        Assert.assertEquals(30, res.intValue());
+    }
+
+    @Test
+    public void inIf() {
+        Boolean boolObj = new Random().nextBoolean();
+
+        if (boolObj) {
+            System.out.println("Ok");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
