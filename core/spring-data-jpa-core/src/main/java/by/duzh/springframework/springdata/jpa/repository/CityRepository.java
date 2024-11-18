3a4da535264813c17538cc507ae30665673e63b1
vduzh <vduzh@tut.by>
2024-11-18 15:48:57 +0300

    working...

--- core/spring-data-jpa-core/src/main/java/by/duzh/springframework/springdata/jpa/repository/CityRepository.java
+++ core/spring-data-jpa-core/src/main/java/by/duzh/springframework/springdata/jpa/repository/CityRepository.java
@@ -2,6 +2,20 @@ package by.duzh.springframework.springdata.jpa.repository;
 
 import by.duzh.springframework.springdata.jpa.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CityRepository extends JpaRepository<City, Integer> {
+    @Query("select ci from Country cn join cn.cities ci where cn.name = :country")
+    List<City> getAllByCountryNameHQ(String country);
+
+    @Query("from City ci join ci.country cn where cn.name = :country")
+    List<City> getAllByCountryName2HQ(String country);
+
+    @Query("from City ci join ci.country cn where cn.name = :country order by ci.name")
+    List<City> getAllByCountryNameOrderedByCityNameHQ(String country);
+
+    @Query("select count(c) from City c")
+    List<City> getCountOfCitiesHQ();
 }
--- core/spring-data-jpa-core/src/main/java/by/duzh/springframework/springdata/jpa/repository/CountryRepository.java
+++ core/spring-data-jpa-core/src/main/java/by/duzh/springframework/springdata/jpa/repository/CountryRepository.java
@@ -2,6 +2,18 @@ package by.duzh.springframework.springdata.jpa.repository;
 
 import by.duzh.springframework.springdata.jpa.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
+    @Query("from Country c")
+    List<Country> getAllHQL();
+
+    @Query("from Country c where c.name = :name")
+    List<Country> getAllByNameHQL(String name);
+
+    // TODO: How???
+    @Query("from Country c order by c.name")
+    List<Country> getListCountriesOrderedByNameHQL(int limit);
 }
