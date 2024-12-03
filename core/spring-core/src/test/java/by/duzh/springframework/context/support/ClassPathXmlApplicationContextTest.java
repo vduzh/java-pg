27a4a63778afc6051c8462d5704932c1a588287b
vduzh <vduzh@tut.by>
2024-12-03 13:19:28 +0300

    introduced setConfigLocation test

--- core/spring-core/src/test/java/by/duzh/springframework/context/support/ClassPathXmlApplicationContextTest.java
+++ core/spring-core/src/test/java/by/duzh/springframework/context/support/ClassPathXmlApplicationContextTest.java
@@ -1,29 +1,42 @@
 package by.duzh.springframework.context.support;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ClassPathXmlApplicationContextTest {
 
+    private static String configLocation;
+
     //new ClassPathXmlApplicationContext("classpath:**/context/" + configLocation)
+
+    @BeforeAll
+    static void setup() {
+        configLocation = ClassPathXmlApplicationContextTest.class
+                .getPackageName().replace('.', '/') + "/app-context-foo.xml";
+    }
 
     @Test
     void create() throws Exception {
-        var configLocation = getClass().getPackageName().replace('.', '/') + "/app-context-foo.xml";
-
         try (var ctx = new ClassPathXmlApplicationContext(configLocation)) {
-            assertTrue(Arrays.asList(ctx.getBeanDefinitionNames()).contains("foo"));
+            List<String> beanDefinitionNames = Arrays.asList(ctx.getBeanDefinitionNames());
+            assertTrue(beanDefinitionNames.contains("foo"));
         }
+    }
 
+    @Test
+    void setConfigLocation() throws Exception {
         try (var ctx = new ClassPathXmlApplicationContext()) {
             ctx.setConfigLocation(configLocation);
             ctx.refresh();
 
-            assertTrue(Arrays.asList(ctx.getBeanDefinitionNames()).contains("foo"));
+            List<String> beanDefinitionNames = Arrays.asList(ctx.getBeanDefinitionNames());
+            assertTrue(beanDefinitionNames.contains("foo"));
         }
     }
 }
