8667c433f59133d1b5c3adef02661c9fa737a6cd
vduzh <viktar.duzh@artezio.com>
2025-06-06 10:42:48 +0300

    spring-cloud-app: added config server

--- app/spring-cloud-app/README.md
+++ app/spring-cloud-app/README.md
@@ -1,18 +1,45 @@
 # Eureka
+
 - http://localhost:8761/ - Eureka
 - TODO: make 2 copies and describe
 
+# Config Server
+
+- https://github.com/vduzh/spring-cloud-app-config - GitHub источник свойств для Config Server
+
+## Foo service
+
+- http://localhost:8888/foo-service/default - конфигурации от Config Server для foo-service если
+  профиль не задан
+- http://localhost:8888/foo-service/prod - конфигурации от Config Server для foo-service для prod
+  профиля
+
+## Bar service
+
+- http://localhost:8888/bar-service/default - конфигурации от Config Server для bar-service если
+  профиль не задан
+
 # GateWay
+
 - http://localhost:8765 - Gateway
 - http://localhost:8765/actuator
+
+## Foo service
+
+- http://localhost:8765/api/v1/foo/foos/info
 - http://localhost:8765/api/v1/foo/foos
+
+## Bar service
+
 - http://localhost:8765/api/v1/bar/bars
 - http://localhost:8765/api/v1/bar/bars/foos
 
 # Services
 
 ## Foo
-- TODO: make 2 instances and describe  
+
+- TODO: make 2 instances and describe
 
 ## Bar
+
 - TODO: make 2 instances and describe
--- app/spring-cloud-app/foo-service/src/main/java/by/duzh/pg/app/spring/cloud/foo/controller/FooController.java
+++ app/spring-cloud-app/foo-service/src/main/java/by/duzh/pg/app/spring/cloud/foo/controller/FooController.java
@@ -2,56 +2,62 @@ package by.duzh.pg.app.spring.cloud.foo.controller;
 
 import by.vduzh.pg.dto.foo.FooDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/foos")
 public class FooController {
 
+    @Autowired
+    private Environment env;
+
     private static final FooDto[] FOO_DATA = {
             new FooDto(1, "Foo 1"),
             new FooDto(2, "Foo 2")
     };
 
-    private final String instanceId;
-
-    public FooController(@Value("${eureka.instance.instance-id}") String instanceId) {
-        this.instanceId = instanceId;
-    }
-
     @GetMapping("/test")
     public String test() {
-        logInstance();
-
         return getClass() + "is working...";
+    }
+
+    @GetMapping("/info")
+    public Map<String, Object> info() {
+        var res = new LinkedHashMap<String, Object>();
+
+        res.put("app.title", env.getProperty("app.title"));
+        res.put("app.timeout", env.getProperty("app.timeout"));
+        res.put("app.db.host", env.getProperty("app.db.host"));
+        res.put("app.db.user", env.getProperty("app.db.user"));
+        res.put("app.db.password", env.getProperty("app.db.password"));
+
+        res.put("foo.title", env.getProperty("foo.title"));
+        res.put("foo.db.url", env.getProperty("foo.db.url"));
+
+        return res;
     }
 
     @GetMapping
     public List<FooDto> getAll() {
-        logInstance();
-
         return List.of(FOO_DATA);
     }
 
     @GetMapping("/{id}")
     public FooDto getFoo(@PathVariable int id) {
-        logInstance();
-
         return Arrays.stream(FOO_DATA)
                 .filter(fooDto -> fooDto.id() == id)
                 .findFirst()
                 .orElse(null);
-    }
-
-    private void logInstance() {
-        log.info("Current instance: {}", instanceId);
     }
 }
